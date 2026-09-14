8b1b6c4f6e8d886cff1defa9166a8099c1de33a6
Vicent149 <mvicentenonato@gmail.com>
2026-09-14 12:42:11 -0300

    codigo de quinta

--- 01_nocoes_e_fundamentos/OlaMundo.py
+++ 01_nocoes_e_fundamentos/OlaMundo.py
@@ -1,2 +1,8 @@
 print ("Vicente Nonato")
 print ("Ola Mundo")
+
+nome = input ("insira seu nome: ")
+mes = input ("Insira o mes em que você nasceu: ")
+ano = input("Insira o ano em que você nasceu: ")
+
+print (f"seu nome é {nome}, você nasceu em {mes} de {ano}")
